0549dc479eb493b2da9c8f326e9967f1e18c72a3
GuXuanDe <534743220@qq.com>
2018-11-23 20:06:40 +0800

    new

--- designCompany/js/accrodion.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".acordeon__head").toggle(function(){
-      $(".acordeon__body").css("height",0);},
-      function(){
-      $("acordeon__body").css("height",'336px');},
-    );
-  });
